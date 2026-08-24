9037c7bfa615727707d12cc75ddb257ffd5f3e25
Techsavy-Maahir <maahirrayaan365@gmail.com>
2026-08-24 22:26:27 +0530

    Implement user authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,18 @@
 const {Router} = require('express')
-
+const authController = require('../controllers/auth.controller')
 const authRouter = Router()
 
+/*
+    @route POST api/auth/register
+    @desc Register a new user
+    @access Public
+*/
+authRouter.post('/register', authController.registerUserController)
 
+/*
+    @route POST api/auth/login
+    @desc Login a usser with email and password
+    @access Public
+*/
+authRouter.post('/login', authController.loginUserController)
 module.exports = authRouter
